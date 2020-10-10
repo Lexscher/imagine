d0068f6812cade57bb497f10686a5fb21ba5f383
Alexander Schelchere <ascellie@gmail.com>
2020-10-10 12:21:48 -0400

    basic rest api configuration

---
+++ cmd/api/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Lexscher/imagine/pkg/application"
+	"github.com/Lexscher/imagine/pkg/exithandler"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("Failed to load env vars")
+	}
+
+	app, err := application.Get()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	exithandler.Init(func() {
+		if err := app.DB.Close(); err != nil {
+			log.Println(err.Error())
+		}
+	})
+}
